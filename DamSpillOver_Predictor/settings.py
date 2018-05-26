5a34dd501ea8b37a34988af108fbd87e24c673f3
Chanaka Rathnakumara <chanaka.2014064@iit.ac.lk>
2018-05-26 17:58:40 +0530

    Updated

--- DamSpillOver_Predictor/settings.py
+++ DamSpillOver_Predictor/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'predictor.apps.PredictorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
